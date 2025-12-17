5e7c99f5e3c34560c2d65bb6289df3a570c6502b
IncuksukEi <incuksuk123@gmail.com>
2025-12-17 15:21:08 +0700

    fix backend_link

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api', // Link Backend của bạn
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-// Thêm Interceptor để tự động gắn Token vào mỗi request (nếu đã đăng nhập)
+
 axiosClient.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token'); // Lấy token từ bộ nhớ trình duyệt
+        const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
