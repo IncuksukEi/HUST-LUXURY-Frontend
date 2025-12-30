81a89a202fd9210a1a76e9a811c53bbdce78f210
IncuksukEi <incuksuk123@gmail.com>
2025-12-30 16:20:26 +0700

    refactor: update axiosClient to use import.meta.env for baseURL

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const baseURL = process.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 const axiosClient = axios.create({
-    baseURL: baseURL,
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 
