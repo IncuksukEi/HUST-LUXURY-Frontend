d161d182836934226cbafc88e4f18e088eb95b1c
IncuksukEi <incuksuk123@gmail.com>
2025-12-30 16:15:34 +0700

    change to process.env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = (import.meta.env.VITE_API_BASE_URL) + "/api";
+const baseURL = process.env.BACKEND_URL || 'http://localhost:5000';
 
 const axiosClient = axios.create({
     baseURL: baseURL,
